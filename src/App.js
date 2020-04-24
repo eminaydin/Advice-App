1738170907d0a6d244ba560e7401569c66aafd12
Emin <eminaydin93@yahoo.com>
2020-04-25 01:49:14 +0200

    Styles changed

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 import React from 'react';
 import './App.css';
 import axios from "axios"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button"
+
 
 class App extends React.Component {
   state = {
-    advice:""
+    advice: ""
   }
-  
+
   componentDidMount() {
-this.fetchAdvice()
+    this.fetchAdvice()
   }
-  
+
   fetchAdvice = () => {
-axios.get("https://api.adviceslip.com/advice")
-.then((res) => {
-  const {advice} = res.data.slip;
-  this.setState({
-    advice
-  });
-console.log(res);
+    axios.get("https://api.adviceslip.com/advice")
+      .then((res) => {
+        const { advice } = res.data.slip;
+        this.setState({
+          advice
+        });
+        console.log(res);
 
-})
-.catch((err) => {
-console.log(err);
+      })
+      .catch((err) => {
+        console.log(err);
 
-})
+      })
   }
   render() {
-    const {advice} = this.state;
+    const { advice } = this.state;
     return (
-    <div className="App">
-     
-     
-     <Card className="card-body">
-  <Card.Header as="h5" className="advice-header">Simple advice app</Card.Header>
-  <Card.Body>
-  
-    <Card.Text>
-    <p className="advice-text">{advice}</p>
-    </Card.Text>
-    <Button variant="primary" onClick={this.fetchAdvice}>Gimme new advice!</Button>
-  </Card.Body>
-</Card>
-    </div>
-  );
-}
+      <div className="App">
+        <div className="card">
+          <h1 className="heading"> {advice} </h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Give me some advice</span>
+          </button>
+
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
